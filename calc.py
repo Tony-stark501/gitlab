f18f457d07ece9f6c2c4452f9a651f5f42493b6a
Vinayaka Subramanyam <165308025+vini-501@users.noreply.github.com>
2024-12-20 22:05:31 +0530

    Created calc.py

---
+++ calc.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 5
+
+sum_res = add_nums(num1,num2)
+print(f"the sum of {num1} and {num2} is: {sum_res}")
+
+mul_res = mul_nums(num1,num2)
+print(f"the product of {num1} and {num2} is: {mul_res}")

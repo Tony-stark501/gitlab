db6e62b2e9577e9ffeed7be023cadd56d9730712
vini <mrcavalier69@gmail.com>
2024-12-20 21:15:08 +0530

    Included add() method

--- calc.c
+++ calc.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 
+<<<<<<< HEAD
 int mul(int a , int b){
   return a*b;
 }
 
+=======
+int add(int a, int b){
+   return a+b;
+}
+
+
+>>>>>>> 85b951e (Included add() method)
 int main(){
 printf("%d",add(3,4));
 printf("%d",mul(15,18));
